6e539dfd87e876174092486bad7cdd6e3df92e72
Paulo Vitor <Teodoropaulo1504@gmail.com>
2025-11-11 22:02:27 -0300

    Criação dos Models do Caça ao Bug 2

---
+++ 02_CacaAoBugMVC/Model/Aluno.cs
@@ -0,0 +1,23 @@
+﻿
+namespace _02_CacaAoBugMVC.Model
+{
+    class Aluno
+    {
+        /*private string nome = string.Empty;
+        //Métodos acessadores
+        public string getNome()
+        {
+            return nome;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }*/
+        //Declaração de propriedades
+        public string Nome { get; set; } = string.Empty;
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public string Situacao { get; set; } = string.Empty;
+    }
+}
---
+++ 02_CacaAoBugMVC/Model/AlunoService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_CacaAoBugMVC.Model
+{
+    class AlunoService
+    {
+        public double CalcularMedia(double n1, double n2, double n3)
+        {
+            return (n1 + n2 + n3) / 3;
+        }
+        public string ObterSituacao(double media)
+        {
+            if(media >= 7)
+                    return "Aprovado";
+            else if (media >= 5)
+                    return "Em Exame Final";
+            else
+                    return "Reprovado";
+
+
+            
+        }
+        public double CalcularTaxaAprovacao(int totalAlunos, int alunosaAprovados)
+        {
+            //Transforma a variavel do tipo int para double utilizando casting
+            return (alunosaAprovados/(double) totalAlunos) * 100.0;
+        }
+
+    }
+}
